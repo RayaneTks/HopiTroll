c4bb0448804e9f19cc2c5e13a80b4affd0e55e50
Rayane BOUDAOUDI <rayanetks13@gmail.com>
2025-05-06 12:19:45 +0200

    Class Maladie OK

--- src/Maladie.java
+++ src/Maladie.java
@@ -1,6 +1,5 @@
 /**
-
- Représente une maladie affectant une créature.
+ * Représente une maladie affectant une créature.
  */
 public class Maladie {
     private String nomComplet;
@@ -9,26 +8,41 @@ public class Maladie {
     private int niveauMax;
 
     /**
-
-     Incrémente le niveau de la maladie.
-
-     Prévoir assert niveauActuel <= niveauMax.
+     * Constructeur de la maladie.
+     * @param nomComplet Nom complet de la maladie
+     * @param nomAbrege Nom abrégé
+     * @param niveauActuel Niveau actuel de gravité
+     * @param niveauMax Niveau maximal avant létalité
      */
-    public void augmenterNiveau() {}
+    public Maladie(String nomComplet, String nomAbrege, int niveauActuel, int niveauMax) {
+        this.nomComplet = nomComplet;
+        this.nomAbrege = nomAbrege;
+        this.niveauActuel = niveauActuel;
+        this.niveauMax = niveauMax;
+    }
 
-    /**
+    /** Incrémente le niveau de la maladie, sans dépasser le niveau max. */
+    public void augmenterNiveau() {
+        if (niveauActuel < niveauMax) {
+            niveauActuel++;
+        }
+    }
 
-     Décrémente le niveau de la maladie.
+    /** Décrémente le niveau de la maladie, sans descendre sous 0. */
+    public void diminuerNiveau() {
+        if (niveauActuel > 0) {
+            niveauActuel--;
+        }
+    }
 
-     Prévoir assert niveauActuel >= 0.
-     */
-    public void diminuerNiveau() {}
+    /** Indique si la maladie est létale (niveau actuel >= niveau max). */
+    public boolean estLetale() {
+        return niveauActuel >= niveauMax;
+    }
 
-    /**
-
-     Indique si la maladie est létale (niveauActuel >= niveauMax).
-
-     @return true si létale, sinon false.
-     */
-    public boolean estLetale() { return false; }
+    // Getters et setters utiles
+    public String getNomComplet() { return nomComplet; }
+    public String getNomAbrege() { return nomAbrege; }
+    public int getNiveauActuel() { return niveauActuel; }
+    public int getNiveauMax() { return niveauMax; }
 }
