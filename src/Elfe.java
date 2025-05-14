503c8d5f101761c6e722901200247f02288e1a2e
Rayane BOUDAOUDI <rayanetks13@gmail.com>
2025-05-14 23:38:14 +0200

    add elfe class

--- src/Elfe.java
+++ src/Elfe.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,8 +9,24 @@ public class Elfe extends Creature {
      * Démoralise une partie des créatures du service lors du trépas.
      * Peut impacter le moral des autres créatures.
      */
-    public void demoraliser() {
+    public Elfe(String nom, String sexe, double poids, double taille, int age, int moral) {
+        this.nomComplet = nom;
+        this.sexe = sexe;
+        this.poids = poids;
+        this.taille = taille;
+        this.age = age;
+        this.moral = moral;
+        this.maladies = new ArrayList<>();
+    }
+
+    public void demoraliser(List<Creature> autresCreatures) {
         // TODO: Implémenter la logique de démoralisation spécifique aux Elfes.
         // assert: le moral des autres créatures doit baisser si un Elfe meurt.
+        System.out.println(nomComplet + " a démoralisé les autres créatures par sa disparition.");
+        for (Creature creature : autresCreatures) {
+            creature.moral -= 10;
+            if (creature.moral < 0) creature.moral = 0;
+        }
+
     }
 }
