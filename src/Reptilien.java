7bf4a344a9676fe06cf8f1a8fc31371e96741caa
Rayane BOUDAOUDI <rayanetks13@gmail.com>
2025-05-14 23:34:48 +0200

    add reptilien class

--- src/Reptilien.java
+++ src/Reptilien.java
@@ -7,7 +7,14 @@ public class Reptilien extends Creature {
     /**
      * Peut démoraliser d'autres créatures (si besoin selon l'énoncé).
      */
-    public void demoraliser() {
-        // TODO: Implémenter la logique de démoralisation si nécessaire.
+    public void demoraliser(List<Creature> autresCreatures) {
+        for (Creature c : autresCreatures) {
+            if (c != this) {  // On ne diminue pas le moral du Reptilien lui-même
+                int ancienMoral = c.moral;
+                c.moral -= 5;  // Diminuer de 5 points, tu peux ajuster la valeur selon tes besoins
+                if (c.moral < 0) c.moral = 0;
+                System.out.println(c.nomComplet + " voit son moral passer de " + ancienMoral + " à " + c.moral);
+            }
+        }
     }
 }
